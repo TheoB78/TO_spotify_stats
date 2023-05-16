1639c2effa61d441317660087cd5a47269d1eda7
tbaustaedter <theo.baustaedter@humanomed-it.at>
2023-05-16 16:55:02 +0200

    added login empty LoginPage view

---
+++ TO-spotify-stats/src/views/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <ion-page>
+        <ion-header>
+            test
+        </ion-header>
+        <ion-content>
+
+        </ion-content>
+    </ion-page>
+</template>
+
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
