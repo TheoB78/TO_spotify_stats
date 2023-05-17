7f29f835ca428facb0e9eec11677f9a7216a913f
tbaustaedter <theo.baustaedter@humanomed-it.at>
2023-05-17 18:39:46 +0200

    Added shims-vue for routing warning messages

--- TO-spotify-stats/src/views/LoginPage.vue
+++ TO-spotify-stats/src/views/LoginPage.vue
@@ -4,13 +4,27 @@
             test
         </ion-header>
         <ion-content>
-
+            <ion-button @click="test()">Login</ion-button>
+            <div>Hallo halli</div>
         </ion-content>
     </ion-page>
 </template>
 
 
-<script setup lang="ts">
+<script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default {
+    name: "login-page",
+
+    methods: {
+        test() {
+           alert('Test') 
+        }
+    },
+    setup() {
+
+    }
+}
 </script>
